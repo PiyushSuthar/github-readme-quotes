132d7597305c9179f0a86d188e1f1fdfa8fb7d33
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-02-07 20:45:28 +0530

    👽 Fixing - Dead API

--- src/fetcher/fetchQuotes.js
+++ src/fetcher/fetchQuotes.js
@@ -1,8 +1,17 @@
 const axios = require('axios').default
 
+// The API is currently down, see issue: https://github.com/skolakoda/programming-quotes-api/issues/37
+// async function fetchQuotes() {
+//     const response = await axios.get('https://programming-quotes-api.herokuapp.com/quotes/random/')
+//     const data = response.data
+//     return parseData(data)
+// }
+
+// Using JSON directly from GitHub Repo.
 async function fetchQuotes() {
-    const response = await axios.get('https://programming-quotes-api.herokuapp.com/quotes/random/')
-    const data = response.data
+    const response = await axios.get('https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/backup/quotes.json')
+//     Randomly Selecting Quote from Array
+    const data = response.data[Math.floor(Math.random() * (json.length + 1))]
     return parseData(data)
 }
 
