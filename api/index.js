30cfcec5b8acdcee3f4abddcc5628434bdaa8fe9
Mini Ware <codauknow@gmail.com>
2021-09-19 17:19:14 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const fetchQuotes = require('../src/fetcher/fetchQuotes')
-const renderSVG = require('../src/renderer/renderSVG')
+const fetchQuotes = require('/home/runner/GitHub-README-Quotes/src/fetcher/fetchQuotes')
+const renderSVG = require('/home/runner/GitHub-README-Quotes/src/renderer/renderSVG')
 
 module.exports = async (req,res)=>{
     const {type} = req.query
@@ -8,4 +8,4 @@ module.exports = async (req,res)=>{
     res.setHeader("Content-Type", "image/svg+xml")
     res.setHeader("Cache-Control", `public, max-age=600`);
     res.send(renderSVG(data, type, theme))
-}
+})
