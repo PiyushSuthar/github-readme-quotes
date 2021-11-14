51a953fdaaebef0341d25dcd115bf94e1d2c167d
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-11-14 22:44:49 +0530

    fix (#10) api not returning response

--- src/fetcher/fetchQuotes.js
+++ src/fetcher/fetchQuotes.js
@@ -1,26 +1,35 @@
 const axios = require('axios').default
 
 // The API is currently down, see issue: https://github.com/skolakoda/programming-quotes-api/issues/37
-// async function fetchQuotes() {
-//     const response = await axios.get('https://programming-quotes-api.herokuapp.com/quotes/random/')
-//     const data = response.data
-//     return parseData(data)
-// }
+async function fetchQuotes() {
+    const response = await axios.get('https://programming-quotes-api.herokuapp.com/quotes/random/')
+    const data = response.data
+
+    let parsedData = parseData(data)
+
+    // check if the quote is less than 150 chars and return
+    return parsedData.quote.length > 150 ? parsedData : fetchQuotes()
+}
 
 // Using JSON directly from GitHub Repo.
-async function fetchQuotes() {
-    const response = await axios.get('https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/backup/quotes.json')
-    const data = response.data
-    //     Randomly Selecting Quote from Array
-    return checkLength(data)
-}
+// async function fetchQuotes() {
+//     const response = await axios.get('https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/backup/quotes.json')
+//     const data = response.data
+//     //     Randomly Selecting Quote from Array
+//     return checkLength(data)
+// }
 
-async function checkLength(data) {
-    let quote = parseData(data[Math.floor(Math.random() * (data.length + 1))])
+// async function checkLength(data) {
+//     let quote = parseData(data[Math.floor(Math.random() * (data.length + 1))])
 
-    return quote.quote.length > 150 ? quote : checkLength(data)
-}
+//     return quote.quote.length > 150 ? quote : checkLength(data)
+// }
 
+/**
+ * 
+ * @param {*} data 
+ * @returns {{quote:string; author:string;}}
+ */
 const parseData = (data) => {
     return {
         quote: data.en,
