381bf6dbaa0cc7398ee8b97907e830783e0bff1a
janaSunrise <warriordefenderz@gmail.com>
2022-01-05 17:48:17 +0530

    Integrate vercel.

--- api/index.js
+++ api/index.js
@@ -2,9 +2,14 @@ const fetchQuotes = require('../src/fetcher/fetchQuotes');
 const renderSVG = require('../src/renderer/renderSVG');
 
 module.exports = async (req, res) => {
+  // Get the queries.
   const { type } = req.query;
   const { theme } = req.query;
-  var data = await fetchQuotes();
+
+  // Get the quotes.
+  let data = await fetchQuotes();
+
+  // Send the quote image response.
   res.setHeader('Content-Type', 'image/svg+xml');
   res.setHeader('Cache-Control', `public, max-age=600`);
   res.send(renderSVG(data, type, theme));
